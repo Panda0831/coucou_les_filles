8e0b0665f306de560727d134e16de77aac48cc10
TheCatWhite <rakotonirainyavotraryan@gmail.com>
2026-04-09 16:30:08 +0300

    amboaro

--- Users/forms.py
+++ Users/forms.py
@@ -5,9 +5,50 @@ from .models import User
 class CustomUserCreationForm(UserCreationForm):
     class Meta(UserCreationForm.Meta):
         model = User
-        fields = ('username', 'email', 'photo')
+        fields = (
+            'username', 
+            'email', 
+            'age', 
+            'date_dernieres_regles', 
+            'taille', 
+            'poids_initial', 
+            'groupe_sanguin', 
+            'nombre_grossesses_precedentes',
+            'photo'
+        )
+        widgets = {
+            'date_dernieres_regles': forms.DateInput(attrs={'type': 'date', 'class': 'form-control'}),
+            'antecedents_medicaux': forms.Textarea(attrs={'rows': 3, 'class': 'form-control'}),
+            'age': forms.NumberInput(attrs={'class': 'form-control'}),
+            'taille': forms.NumberInput(attrs={'class': 'form-control'}),
+            'poids_initial': forms.NumberInput(attrs={'class': 'form-control'}),
+            'groupe_sanguin': forms.Select(attrs={'class': 'form-control'}),
+            'username': forms.TextInput(attrs={'class': 'form-control'}),
+            'email': forms.EmailInput(attrs={'class': 'form-control'}),
+        }
 
 class CustomUserChangeForm(UserChangeForm):
-    class Meta(UserChangeForm.Meta):
+    # On retire le mot de passe du formulaire de modification simple
+    password = None 
+
+    class Meta:
         model = User
-        fields = ('username', 'email', 'photo')
+        fields = (
+            'username', 
+            'email', 
+            'age', 
+            'date_dernieres_regles', 
+            'taille', 
+            'poids_initial', 
+            'groupe_sanguin', 
+            'antecedents_medicaux',
+            'photo'
+        )
+        widgets = {
+            'date_dernieres_regles': forms.DateInput(attrs={'type': 'date', 'class': 'form-control'}),
+            'age': forms.NumberInput(attrs={'class': 'form-control'}),
+            'taille': forms.NumberInput(attrs={'class': 'form-control'}),
+            'poids_initial': forms.NumberInput(attrs={'class': 'form-control'}),
+            'groupe_sanguin': forms.Select(attrs={'class': 'form-control'}),
+            'antecedents_medicaux': forms.Textarea(attrs={'rows': 3, 'class': 'form-control'}),
+        }
--- Users/models.py
+++ Users/models.py
@@ -1,9 +1,26 @@
+from datetime import timedelta
+
 from django.contrib.auth.models import AbstractUser
 from django.db import models
 
 class User(AbstractUser):
+    GROUPE_SANGUIN_CHOIX = [
+        ('A+', 'A+'), ('A-', 'A-'),
+        ('B+', 'B+'), ('B-', 'B-'),
+        ('AB+', 'AB+'), ('AB-', 'AB-'),
+        ('O+', 'O+'), ('O-', 'O-'),
+    ]
     is_admin_user = models.BooleanField(default=False)
     is_customer = models.BooleanField(default=True)
+    age = models.PositiveIntegerField(null=True, blank=True,help_text="25")
+    taille = models.PositiveIntegerField(null=True, blank=True, help_text="En cm")
+    date_dernieres_regles = models.DateField(null=True)
+    date_prevue_accouchement = models.DateField(null=True, blank=True)
+    taille = models.PositiveIntegerField(null=True, blank=True, help_text="En cm")
+    poids_initial = models.FloatField(null=True,help_text="En kg")
+    groupe_sanguin = models.CharField(null=True, blank=True, max_length=5, choices=GROUPE_SANGUIN_CHOIX)
+    antecedents_medicaux = models.TextField(null=True,blank=True)
+    nombre_grossesses_precedentes = models.PositiveIntegerField(default=0)
     photo = models.ImageField(
         upload_to="profile_pictures/",
         default="profile_pictures/default.png",
@@ -13,6 +30,21 @@ class User(AbstractUser):
 
     def __str__(self):
         return self.username
+    
+    def accouchement(self):
+        if self.date_dernieres_regles:
+            return self.date_dernieres_regles + timedelta(days=280)
+        return None
+    @property
+    def semaine_actuelle(self):
+        import datetime
+        delta = datetime.date.today() - self.date_dernieres_regles
+        return delta.days // 7
+    
+    def save(self, *args, **kwargs):
+        if not self.date_prevue_accouchement:
+            self.date_prevue_accouchement = self.accouchement()
+        super().save(*args, **kwargs)
 
 class ChatMessage(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="messages")
